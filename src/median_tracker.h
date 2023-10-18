48869ffaa6771fd6705654c41fe633aeee9a3cf5
clark <chihung.tzeng@gmail.com>
2023-10-18 20:35:00 +0800

    Add median_tracker

---
+++ src/median_tracker.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+
+// Work In Progress
+class IndexedPriorityQueue;
+
+class MedianTracker {
+ public:
+  MedianTracker(int window_size);
+  ~MedianTracker(){};
+  int add(double val);
+  int remove(double val);
+  double median();
+
+ private:
+  int window_size_;
+  std::unique_ptr<IndexedPriorityQueue> max_ipq_ptr_, min_ipq_ptr_;
+};
